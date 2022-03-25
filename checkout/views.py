69c0c97cdf4bb8878e0492c48b08ede01746fc8e
Luke Bailey <baileyl1991@gmail.com>
2022-03-25 21:22:39 +0000

    fix a couple of typos

--- checkout/views.py
+++ checkout/views.py
@@ -11,7 +11,7 @@ def checkout(request):
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('products'))
 
-    order_form = order_form()
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
