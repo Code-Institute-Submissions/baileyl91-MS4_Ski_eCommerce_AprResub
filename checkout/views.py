61d65145fe6cb9a19d9c877948dc078e639ef116
Luke Bailey <baileyl1991@gmail.com>
2022-03-26 11:18:24 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KhXNnCPqKgZFUjKJtAsldVllD71X0WeO3xsJwAtehAymCrMJA5oNLxaSxGQkH8VkW5nKXo6jWTsDKbHszsCDMvW00rG7xOukm',
+        'client_secrect': 'test client secrect',
     }
 
     return render(request, template, context)
