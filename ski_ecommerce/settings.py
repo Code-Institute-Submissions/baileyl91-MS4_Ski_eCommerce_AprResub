bef052afa1fbd5a1c3b24a8badbe3caa4c21c141
Luke Bailey <baileyl1991@gmail.com>
2022-03-29 20:41:44 +0000

    add whitenoise to middleware

--- ski_ecommerce/settings.py
+++ ski_ecommerce/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
@@ -163,7 +164,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 
 # Static files (CSS, JavaScript, Images)
