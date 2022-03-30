affda3db0e943f7d1505a8bb5589391b1897dcda
Luke Bailey <baileyl1991@gmail.com>
2022-03-30 08:12:05 +0000

    added cache control

--- ski_ecommerce/settings.py
+++ ski_ecommerce/settings.py
@@ -189,6 +189,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ski-ecommerce'
     AWS_S3_REGION_NAME = 'EU (London) eu-west-2'
@@ -206,7 +212,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 
 # Stripe
