60886a9947bcc24ccdcc3e8edc772beef7a4cbef
Luke Bailey <baileyl1991@gmail.com>
2022-04-14 17:13:51 +0000

    add sizes to orderlineitem

--- checkout/models.py
+++ checkout/models.py
@@ -66,7 +66,10 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    
+    has_sizes = models.BooleanField(null=True, blank=True, default=False)
+    has_ski_sizes = models.BooleanField(null=True, blank=True, default=False)
+    has_boots_sizes = models.BooleanField(null=True, blank=True, default=False)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
